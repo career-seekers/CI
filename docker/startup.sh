464ec501fddcb12d0a97ca351cdca348df0f499d
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-06-07 00:01:50 +0300

    build(core): created docker container

---
+++ docker/startup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+mkdir /app
+
+apt-get update && apt-get install -y git
+
+cd /app
+
+git clone https://github.com/career-seekers/CI.git .
+
+./gradlew build
+
+cd build/libs
+
+chmod +x cs-ci-0.0.1-SNAPSHOT.jar
+
+java -jar cs-ci-0.0.1-SNAPSHOT.jar
