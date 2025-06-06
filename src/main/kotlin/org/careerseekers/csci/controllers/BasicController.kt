1338f0b77abb06c158c54438a7c3a15ca2930a24
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-06-07 00:40:39 +0300

    feat(core): created test endpoint

--- src/main/kotlin/org/careerseekers/csci/controllers/BasicController.kt
+++ src/main/kotlin/org/careerseekers/csci/controllers/BasicController.kt
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class BasicController {
-    @GetMapping
+    @GetMapping("/")
     fun hello(): String {
         return "Hello, world! Service working correctly"
     }
