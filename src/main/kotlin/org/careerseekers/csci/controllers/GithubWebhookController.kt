6e8a103585115c171601089d1ad719502a630486
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-06-11 23:35:04 +0300

    fix(core): small fixes with ci working process

--- src/main/kotlin/org/careerseekers/csci/controllers/GithubWebhookController.kt
+++ src/main/kotlin/org/careerseekers/csci/controllers/GithubWebhookController.kt
@@ -1,6 +1,6 @@
 package org.careerseekers.csci.controllers
 
-import org.careerseekers.csci.ContainerService
+import org.careerseekers.csci.services.ContainerService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
--- src/main/kotlin/org/careerseekers/csci/services/ContainerService.kt
+++ src/main/kotlin/org/careerseekers/csci/services/ContainerService.kt
@@ -1,4 +1,4 @@
-package org.careerseekers.csci
+package org.careerseekers.csci.services
 
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
@@ -9,7 +9,7 @@ class ContainerService {
     @Async
     fun updateContainers(container: String) {
         try {
-            val pb = ProcessBuilder("docker", "restart", container)
+            val pb = ProcessBuilder("/usr/bin/docker", "restart", container)
             pb.inheritIO().start().waitFor()
         } catch (e: Exception) {
             e.printStackTrace()
