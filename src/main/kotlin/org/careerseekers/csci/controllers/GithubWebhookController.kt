0409dc22ae6e4b54fb30c189b4f803e4616fb401
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-06-11 23:43:19 +0300

    test(core): testing ci working

--- src/main/kotlin/org/careerseekers/csci/controllers/GithubWebhookController.kt
+++ src/main/kotlin/org/careerseekers/csci/controllers/GithubWebhookController.kt
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-
 @RestController
 @RequestMapping("/webhook/github")
 class GithubWebhookController(private val containerService: ContainerService) {
